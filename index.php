1b7b6b79cbb9f3859b254a2727c8f32b2e4f7003
NEON <mgutie33@hotmail.com>
2022-10-28 20:27:52 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+print_r("<br><br><br>Prueba backend<br><br>");
+
+echo "bchjbcds";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title> PAGINA ALUMNO</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="PTJmEJtZHVFFDsYa6v5h401LBzTGws9ehqdVm-IrCiY" />
+        <link href="css/style.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    </head>
+    <body>
+
+        <div style="z-index: 2; position: absolute; width: 200px; bottom: 40px; top: 70px; left: 200px; background-color: red; overflow-y: scroll;">
+            prueba frontend
+        </div>
+
+    </body>
+</html>
